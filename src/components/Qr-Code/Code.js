ffd4c0d29ebb9ccffe2ed45e2b0a30b60bbdab3f
Nhoj <nhojplummer@yahoo.com>
2023-06-08 15:02:38 -0400

    Made sctions for the text goona set it up

--- src/components/Qr-Code/Code.js
+++ src/components/Qr-Code/Code.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import './code.css'
+import React from "react"
+import "./code.css"
 
 function Code() {
   return (
-    <div className='qr__code-container section_padding'>
-      <div className='qr__code-content'>
-
+    <div className="qr__code-container section_padding">
+      <div className="qr__code-content">
+        <div className="qr__code-image">
+          <img />
+        </div>
+        <div className="qr__code-text">
+          <h2>Improve your front-end skills by building projects</h2>
+          <p></p>
+        </div>
       </div>
     </div>
   )
