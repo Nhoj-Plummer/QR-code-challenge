86c01eaa2a2db213f7ff0d389ad76faa5d7995f0
Nhoj <nhojplummer@yahoo.com>
2023-06-10 15:45:29 -0400

    Text section built

--- src/components/Qr-Code/Code.js
+++ src/components/Qr-Code/Code.js
@@ -6,15 +6,14 @@ function Code() {
     <div className="qr__code-container section_padding">
       <div className="qr__code-content">
         <div className="qr__code-image">
-          <img src="./assets/qr-code.png" alt="code"/>
+          <img src="./assets/qr-code.png" alt="code" />
         </div>
         <div className="qr__code-text">
           <h2>Improve your front-end skills by building projects</h2>
         </div>
         <div className="qr__code-subtext">
-        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+          <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
         </div>
-       
       </div>
     </div>
   )
