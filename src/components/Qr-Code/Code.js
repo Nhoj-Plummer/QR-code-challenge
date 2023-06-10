a8965bc5df8cc7d760be87acd595a9fdd0cfff91
Nhoj <nhojplummer@yahoo.com>
2023-06-10 15:13:53 -0400

    Continuing progress

--- src/components/Qr-Code/Code.js
+++ src/components/Qr-Code/Code.js
@@ -10,8 +10,11 @@ function Code() {
         </div>
         <div className="qr__code-text">
           <h2>Improve your front-end skills by building projects</h2>
-          <p></p>
         </div>
+        <div className="qr__code-subtext">
+        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
+       
       </div>
     </div>
   )
